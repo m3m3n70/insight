89633f2e7994cf388b1b7403da51d831065c6333
Keenahn Jung <keenahn@gmail.com>
2015-10-10 15:23:00 -0700

    Emit for multiple clients but keep one connection to Asana so as to not flood the rate limiter

--- server.js
+++ server.js
@@ -11,45 +11,77 @@ var projectId = 52963906013475;
 var client = asana.Client.create().useBasicAuth(apiKey);
 var taskCollection = [];
 
-app.use(express.static(__dirname + "/public"));
 
-server.listen(process.env.PORT, function(){
-  console.log('Listening at port ' + process.env.PORT);
-});
+var allClients = [];
+
+var emitAllClients = function(event, data){
+  for(var i = 0; i < allClients.length; i++){
+    if(allClients[i]){
+      allClients[i].emit(event, data)
+    }
+  }
+}
+
+var tasks = {};
 
 // TODO: move these to another file
+
+var asanaListening = false;
+
 var setupAsanaListener = function(socket){
+  if(asanaListening){
+    return true;
+  }
   var readable = client.events.stream(projectId, {periodSeconds: 3});
-  var tasks = {};
   readable.on('data', function(item) {
     // console.log(item);
     if(item["type"] === "task" && item["resource"]["name"].length > 0){
       // console.log(Math.floor(Date.now() / 1000))
       if(tasks[item["resource"]["id"]]){
         // Task exists already, so this was an update and not an add
-        socket.emit('task-updated', item["resource"]);
+        emitAllClients('task-updated', item["resource"]);
       } else {
-        socket.emit('task-added', item["resource"]);
+        emitAllClients('task-added', item["resource"]);
       }
       tasks[item["resource"]["id"]] = item["resource"]["name"];
     }
   });
+  asanaListening = true;
 }
 
 var loadInitialTasks = function(socket){
+  // if(taskCollection.length > 0){
+  //   socket.emit("initial-tasks-loaded", taskCollection);
+  // } else {
   client.tasks.findByProject(projectId, {completed_since: "now", limit: 50}).then(function(collection) {
     taskCollection = collection.data;
-    socket.emit("initial-tasks-loaded", collection.data);
+    socket.emit("initial-tasks-loaded", taskCollection);
   });
+  // }
 }
 
 var handleSocketConnection = function (socket) {
   var _socket = socket;
-  console.log('Someone connected');
+  allClients.push(_socket);
+  console.log(allClients.length + " users connected");
   loadInitialTasks(_socket);
   setupAsanaListener(_socket);
+
+  socket.on('disconnect', function() {
+    console.log('Got disconnect!');
+
+    var i = allClients.indexOf(socket);
+    delete allClients[i];
+    console.log(allClients.length + " users connected");
+  });
+
 }
 
-io.sockets.on('connection', handleSocketConnection);
+app.use(express.static(__dirname + "/public"));
 
+server.listen(process.env.PORT, function(){
+  console.log('Listening at port ' + process.env.PORT);
+});
+
+io.sockets.on('connection', handleSocketConnection);
 
