44682dc6a4f42b430d1b7e04d6c5af0389fc73e1
Keenahn Jung <keenahn@gmail.com>
2015-10-13 06:33:24 -0700

    Removed some cruft

--- public/app.js
+++ public/app.js
@@ -1,61 +1,23 @@
 var app = angular.module('insight', [ 'socket.io', 'ngAnimate' ]);
 
 app.config(function ($socketProvider) {
-
   var url = "/";
   if(window.location.host.match(/localhost/)){
     url = "http://localhost:8080";
   }
-
-  // console.log(url);
 
   $socketProvider.setConnectionUrl(url);
 });
 
 
 // TODO: move to another file
-
 var mainController = function ($scope, $timeout, $socket, InsightFactory) {
 
   $scope.tasks = [];
-
   $scope.projects = null;
-
   $scope.loaded = false;
-  // var styles = [
-  //   "one",
-  //   "two",
-  //   "three",
-  //   "four",
-  //   "five",
-  //   "six"
-  // ];
-  // Listen to task added events
-  // $socket.on('task-added', function (data) {
-  //   console.log("Added task")
-  //   var project = data.project;
-  //   var task = data.task;
-  //   var projIndex = projectIdMap[project.id];
-  //   $scope.projectsWithTasks[projIndex].tasks.unshift(task);
-  // });
 
-  var projectIdMap = {};
-
-  // Grab the initial projects with their tasks
-  function loadProjectsWithTasks(){
-    InsightFactory.projectsWithTasks().then(function(response){
-      $timeout(function(){
-        var i = 0;
-        $scope.projectsWithTasks = response.data.map(function(project){
-          project.cssClass = styles[i % 6];
-          projectIdMap[project.id] = i;
-          i++;
-          return project;
-        });
-      });
-    })
-  }
-
+  // TODO: move to a constant
   asanaColors = {
     "dark-pink": "#b13f94",
     "dark-green": "#427e53",
@@ -93,12 +55,9 @@ var mainController = function ($scope, $timeout, $socket, InsightFactory) {
 
     var chartData = [];
     var chartColors = {};
-    var diameter = 0;
-
     var projects = team["projects"];
     for(var i = 0; i < projects.length; i++){
       var project = projects[i];
-      diameter += parseInt(project["taskCount"]);
       chartData.push(
         [
           project["name"],
@@ -108,15 +67,7 @@ var mainController = function ($scope, $timeout, $socket, InsightFactory) {
       chartColors[project["name"]] = asanaColors[project["color"]]
     }
 
-    // diameter = Math.min(200, diameter);
-
-    // 1920 x 1080
-
     var bindTo = '#team-' + team["id"] + " .pie-chart";
-
-    // console.log(bindTo);
-    // console.log(chartData);
-    // console.log(chartColors);
 
     var chart = c3.generate({
         bindto: '#team-' + team["id"] + " .pie-chart",
@@ -137,49 +88,33 @@ var mainController = function ($scope, $timeout, $socket, InsightFactory) {
     team.chart = chart;
 
     return chart;
-  } ;
+  }
 
   function updateOnHeartbeat(heartbeat){
-
-    // team = heartbeat[1];
-    // generateChartForTeam(team);
-
-    console.log(heartbeat);
-
     var maxWidth = 1080.0;
-    var minWidth = 200;
-
     var totalTaskCount = 0.0;
     var team, chart, diameter;
 
 
-    // Now, scale all the charts again based on the totalTaskCount
-
+    // Set the totalTaskCount
     for(var i = 0; i < heartbeat.length ; i++){
       team = heartbeat[i];
       totalTaskCount += parseInt(team["taskCount"]);
     }
 
+    // Calculate the diameter as a percentage of the total task count
     for(i = 0; i < heartbeat.length ; i++){
       team = heartbeat[i];
-      console.log(team["taskCount"]);
-      console.log(totalTaskCount);
       diameter = (parseInt(team["taskCount"]) / totalTaskCount) * maxWidth;
-      console.log(team.name);
-      console.log(diameter);
       team.diameter = diameter;
 
-
+      // Then render the chart
       generateChartForTeam(team);
     }
-
-
-
-  } ;
+  }
 
 
   $socket.on("heartbeat", function (data) {
-    // console.log("heartbeat");
     $scope.loaded = true;
     updateOnHeartbeat(data);
   });
@@ -196,7 +131,7 @@ mainController.$inject = [
 angular.module('insight').controller('mainController', mainController);
 
 // TODO: move to another file
-
+// Currently unused since we have one socket that just listens for heartbeats
 var InsightFactory = function ($http){
   var projects = function(){
     return $http.get("/projects");
